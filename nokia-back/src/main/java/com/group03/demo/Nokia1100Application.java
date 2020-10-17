e0783c37b29e43518aa18e39c6c35336ea1be351
Gabriel Vieira <gabrielvieira1@protonmail.com>
2020-10-17 15:58:22 -0300

    Projeto criado

---
+++ nokia-back/src/main/java/com/group03/demo/Nokia1100Application.java
@@ -0,0 +1,14 @@
+package com.group03.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nokia1100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nokia1100Application.class, args);
+		
+	}
+
+}
---
+++ nokia-back/src/test/java/com/group03/demo/Nokia1100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group03.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nokia1100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
